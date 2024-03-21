6f12377ced07d595fefe10f6a963162843b728cc
stopping <1064076070@qq.com>
2024-03-21 23:50:50 +0800

    feat:归并算法

---
+++ src/main/java/com/company/sort/MergeSort.java
@@ -0,0 +1,64 @@
+package com.company.sort;
+
+/**
+ * 归并排序
+ */
+public class MergeSort {
+    // 用于辅助合并有序数组
+    private static int[] temp;
+
+    public static void main(String[] args) {
+        int i []  = {8,3,4,7,1,5,2,6};
+        sort(i);
+    }
+
+    public static void sort(int[] nums) {
+        // 先给辅助数组开辟内存空间
+        temp = new int[nums.length];
+        // 排序整个数组（原地修改）
+        sort(nums, 0, nums.length - 1);
+    }
+
+    // 定义：将子数组 nums[lo..hi] 进行排序
+    private static void sort(int[] nums, int lo, int hi) {
+        if (lo == hi) {
+            // 单个元素不用排序
+            return;
+        }
+        // 这样写是为了防止溢出，效果等同于 (hi + lo) / 2
+        int mid = lo + (hi - lo) / 2;
+        // 先对左半部分数组 nums[lo..mid] 排序
+        sort(nums, lo, mid);
+        // 再对右半部分数组 nums[mid+1..hi] 排序
+        sort(nums, mid + 1, hi);
+        // 将两部分有序数组合并成一个有序数组
+        merge(nums, lo, mid, hi);
+    }
+
+    // 将 nums[lo..mid] 和 nums[mid+1..hi] 这两个有序数组合并成一个有序数组
+    private static void merge(int[] nums, int lo, int mid, int hi) {
+        // 先把 nums[lo..hi] 复制到辅助数组中
+        // 以便合并后的结果能够直接存入 nums
+        for (int i = lo; i <= hi; i++) {
+            temp[i] = nums[i];
+        }
+
+        // 数组双指针技巧，合并两个有序数组
+        // 因为分组是l->mid,mid+1 -> h,所以在这两组数组中都是相对有序的
+        // 故两个指针从i,j = mid+1开始比较，如果t[i] > t[j] ,N[p] = t[j++],t[i] < t[j] ,N[p] = t[I++],P指针从l，h遍历
+        int i = lo, j = mid + 1;
+        for (int p = lo; p <= hi; p++) {
+            if (i == mid + 1) {
+                // 左半边数组已全部被合并。有半区均大于左半区
+                nums[p] = temp[j++];
+            } else if (j == hi + 1) {
+                // 右半边数组已全部被合并
+                nums[p] = temp[i++];
+            } else if (temp[i] > temp[j]) {
+                nums[p] = temp[j++];
+            } else {
+                nums[p] = temp[i++];
+            }
+        }
+    }
+}
